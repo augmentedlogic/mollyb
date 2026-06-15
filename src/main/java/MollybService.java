70e40748e1ec3d8539839f3e06c738b8912ac639
Wolfgang Hauptfleisch <wh@augmentedlogic.com>
2026-06-15 13:51:51 +0100

    version bump

--- src/main/java/MollybService.java
+++ src/main/java/MollybService.java
@@ -29,7 +29,7 @@ import java.util.Properties;
 public class MollybService {
 
 
-    public static final String VERSION = "0.6";
+    public static final String VERSION = "0.7";
 
     private String bind_to = "localhost";
     private int port = 8080;
